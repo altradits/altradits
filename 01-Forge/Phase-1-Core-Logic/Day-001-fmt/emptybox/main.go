decd1cb712403a35cd5383d4ff3815850179f687
altradits <altradits@gmail.com>
2026-05-02 01:07:44 +0300

    if a string is not declaired, Go assigns it a zero

---
+++ 01-Forge/Phase-1-Core-Logic/Day-001-fmt/emptybox/main.go
@@ -0,0 +1,10 @@
+// Empty Box: Declare a string variable message without giving it a value, then print it (See what Go's default is).
+package main
+
+import "fmt"
+
+func main() {
+	var message string
+
+	fmt.Println(message)
+}
