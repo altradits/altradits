3cab78a08e65b278d11e1279aa768de04129972c
altradits <altradits@gmail.com>
2026-05-02 00:49:24 +0300

    The Joiner: Print Part A and Part B on the same line using one fmt.Print for each.

---
+++ 01-Forge/Phase-1-Core-Logic/Day-001-fmt/joiner/main.go
@@ -0,0 +1,9 @@
+// The Joiner: Print "Part A" and "Part B" on the same line using one fmt.Print for each.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Part A")
+	fmt.Print("Part B")
+}
