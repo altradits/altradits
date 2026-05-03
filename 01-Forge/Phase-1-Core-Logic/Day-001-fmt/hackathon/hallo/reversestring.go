97cf648e686ecd61c6f6d94eb0e1e54c33528cec
altradits <altradits@gmail.com>
2026-05-03 20:08:01 +0300

    feat: Add func reverse string

---
+++ 01-Forge/Phase-1-Core-Logic/Day-001-fmt/hackathon/hallo/reversestring.go
@@ -0,0 +1,14 @@
+// Write a function that takes a string as an input and returns that string reversed.
+package main
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	println(reverseString("Surgical Go"))
+}
