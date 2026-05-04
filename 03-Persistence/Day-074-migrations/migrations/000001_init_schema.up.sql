07bf0a10baca332f706462d7bf6be73848e0ebf2
altradits <altradits@gmail.com>
2026-05-04 19:11:29 +0300

    feat: learn Day 1 and 2

--- 03-Persistence/Day-074-migrations/migrations/000001_init_schema.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS transactions (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    sender_name VARCHAR(255) NOT NULL,
-    recipient_name VARCHAR(255) NOT NULL,
-    amount DECIMAL(15, 2) NOT NULL,
-    status VARCHAR(50) DEFAULT 'pending',
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
