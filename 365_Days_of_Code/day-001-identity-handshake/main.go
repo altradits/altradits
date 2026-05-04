0bc73ac47333e526858ba2c7cb746e02a57bbc71
altradits <altradits@gmail.com>
2026-05-04 10:32:24 +0300

    fix: printf to declair string varriables as explicit values %s

--- 365_Days_of_Code/day-001-identity-handshake/main.go
+++ 365_Days_of_Code/day-001-identity-handshake/main.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// Using explicit values to ensure Mathematical Absolute truth in identity
 	name := "Stanley Chege Thuita"
 	role := "Principal Architect"
-	fmt.Println("[ALTRADITS KERNEL] Welcome", name, "Access granted as", role)
+
+	// Printf allows for surgical string formatting
+	fmt.Printf("[ALTRADITS KERNEL] Welcome %s. Access granted as %s\n", name, role)
 }
