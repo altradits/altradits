27a7e92646f7d9400a0c089565d3d598ab1cabb4
altradits <altradits@gmail.com>
2026-05-04 10:18:15 +0300

    feat: declaire variable and call them

---
+++ 365_Days_of_Code/day-001-identity-handshake/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Stanley Chege Thuita"
+	role := "Principal Architect"
+	fmt.Println("[ALTRADITS KERNEL] Welcome", name, "Access granted as", role)
+}
