cddba83f8b5d25ed1b5cc280be1c20227fec71da
altradits <altradits@gmail.com>
2026-05-17 19:06:37 +0300

    feat: Added Vault to store core engine ledger

---
+++ internal/ledger/vault.go
@@ -0,0 +1,15 @@
+package ledger
+
+import "fmt"
+
+func RecordTransaction(initialBalance int64, creditAmount int64, debitAmount int64) {
+
+	fmt.Println("CORE ENGINE LEDGER")
+
+	finalBalance := initialBalance + creditAmount - debitAmount
+
+	fmt.Printf("Initial Balance: %.2f/n", float64(initialBalance)/100)
+	fmt.Printf("Credit Push: +%.2f/n", float64(creditAmount)/100)
+	fmt.Printf("Initial Pull: -%.2f/n", float64(creditAmount)/100)
+	fmt.Printf("Final Balance: %.2f/n", float64(finalBalance)/100)
+}
